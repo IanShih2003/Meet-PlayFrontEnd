97638c095d097e1bf412cc9ba1f485664b503973
palmax555 <39124740+palmax555@users.noreply.github.com>
2020-06-26 22:37:46 -0300

    Login Prueba First Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{marginLeft: 685}}><input name="name" placeholder="Ingresar Nombre"/></div>
+      <div style={{marginLeft: 685}}><input name="email" placeholder="Ingresar E-mail"/></div>
+      <div style={{marginLeft: 685}}><input name="password" placeholder="Ingresar Contraseña"/></div>
+
+      <button style={{marginLeft: 685}}>Registrarse</button>
     </div>
+    
   );
 }
 
